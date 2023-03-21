0697026c29010594e09fb0bde1ee095f9ace28ba
boticelli <kandelakitina@gmail.com>
2023-03-21 18:20:14 +0300

    Use TELEGRAM_TESTING APIs instead of main

--- .github/workflows/update-env-file.sh
+++ .github/workflows/update-env-file.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Set the variables
-TELEGRAM_TESTING_API_KEY="${{ secrets.TELEGRAM_TESTING_API_KEY }}"
+TELEGRAM_API_KEY="${{ secrets.TELEGRAM_TESTING_API_KEY }}"
 OPENAI_API_KEY="${{ secrets.OPENAI_API_KEY }}"
 
 # Check if the variables exist in the .env file
-if grep -q "^TELEGRAM_TESTING_API_KEY=" .env; then
+if grep -q "^TELEGRAM_API_KEY=" .env; then
   # Update the existing variable in the .env file
-  sed -i "s/^TELEGRAM_TESTING_API_KEY=.*/TELEGRAM_TESTING_API_KEY=${TELEGRAM_TESTING_API_KEY}/" .env
+  sed -i "s/^TELEGRAM_API_KEY=.*/TELEGRAM_API_KEY=${TELEGRAM_API_KEY}/" .env
 else
   # Add the new variable to the .env file
-  echo "TELEGRAM_TESTING_API_KEY=${TELEGRAM_TESTING_API_KEY}" >> .env
+  echo "TELEGRAM_API_KEY=${TELEGRAM_API_KEY}" >> .env
 fi
 
 if grep -q "^OPENAI_API_KEY=" .env; then
