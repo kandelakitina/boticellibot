9c00e070eabf39c848471eaa632832ee8176c94d
boticelli <kandelakitina@gmail.com>
2023-03-21 18:00:41 +0300

    Create testing bot

---
+++ .github/workflows/update-env-file.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set the variables
+TELEGRAM_TESTING_API_KEY="${{ secrets.TELEGRAM_TESTING_API_KEY }}"
+OPENAI_API_KEY="${{ secrets.OPENAI_API_KEY }}"
+
+# Check if the variables exist in the .env file
+if grep -q "^TELEGRAM_TESTING_API_KEY=" .env; then
+  # Update the existing variable in the .env file
+  sed -i "s/^TELEGRAM_TESTING_API_KEY=.*/TELEGRAM_TESTING_API_KEY=${TELEGRAM_TESTING_API_KEY}/" .env
+else
+  # Add the new variable to the .env file
+  echo "TELEGRAM_TESTING_API_KEY=${TELEGRAM_TESTING_API_KEY}" >> .env
+fi
+
+if grep -q "^OPENAI_API_KEY=" .env; then
+  # Update the existing variable in the .env file
+  sed -i "s/^OPENAI_API_KEY=.*/OPENAI_API_KEY=${OPENAI_API_KEY}/" .env
+else
+  # Add the new variable to the .env file
+  echo "OPENAI_API_KEY=${OPENAI_API_KEY}" >> .env
+fi
+
