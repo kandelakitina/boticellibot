1248aaffe3054d4defc0279953beab917c61177d
Mikhail Eremeneko <eremenko86@gmail.com>
2023-07-09 09:05:56 +0300

    Add OpenAI overload error catch

--- bot.js
+++ bot.js
@@ -18,31 +18,30 @@ bot.onText(/\/start/, (msg) => {
 
 
 const globalHistory = {};
-
 bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-  const localHistory = globalHistory[chatId] || [];
-  localHistory.push(msg.text);
-  globalHistory[chatId] = localHistory;
+  try {
+    const chatId = msg.chat.id;
+    const localHistory = globalHistory[chatId] || [];
+    localHistory.push(msg.text);
+    globalHistory[chatId] = localHistory;
 
-  console.log(`History -5:${localHistory[localHistory.length - 5]}`);
-  console.log(`History -4:${localHistory[localHistory.length - 4]}`);
-  console.log(`History -3:${localHistory[localHistory.length - 3]}`);
-  console.log(`History -2:${localHistory[localHistory.length - 2]}`);
-  console.log(`Current: ${msg.text}\n`);
+    console.log(`History -5:${localHistory[localHistory.length - 5]}`);
+    console.log(`History -4:${localHistory[localHistory.length - 4]}`);
+    console.log(`History -3:${localHistory[localHistory.length - 3]}`);
+    console.log(`History -2:${localHistory[localHistory.length - 2]}`);
+    console.log(`Current: ${msg.text}\n`);
 
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "Ассистент - остроумный друг-юморист пользователя, который отвечает коротко и хлестко. Максимум одно-два предложения.",
-      },
-      {
-        role: "user",
-        content: "Привет! как дела?",
-      },
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+	{
+	  role: "system",
+	  content: "Ассистент - остроумный друг-юморист пользователя, который отвечает к>"
+	},
+	{
+	  role: "user",
+	  content: "Привет! как дела?",
+	},
       {
         role: "assistant",
         content: "Йо! Здаров, чувак, да вроде зашибок. Сам как?",
@@ -82,4 +81,12 @@ bot.on("message", async (msg) => {
 
   // Send the generated text back to the user
   bot.sendMessage(chatId, response.data.choices[0].message.content);
+	    } catch (error) {
+    console.error(error);
+    if (error.message.includes("overloaded with other requests")) {
+      bot.sendMessage(msg.chat.id, "ChatGPT is over capacity, try a bit later.");
+    } else {
+      bot.sendMessage(msg.chat.id, "Something went wrong. Please try again later.");
+    }
+  }
 });
